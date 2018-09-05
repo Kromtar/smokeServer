ffb5c7473592249e145bb4b12d3b7b71ee609230
Federico Jensen Carrasco <federicodaniel25@gmail.com>
2018-09-05 18:30:57 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+  console.log('Server ON, port:', process.env.PORT);
+  console.log('The environment is', process.env.NODE_ENV);
+});
