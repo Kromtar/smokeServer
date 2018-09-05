5421fc65d27f8605dc4a161b5d16036c4e5017cc
Federico Jensen Carrasco <federicodaniel25@gmail.com>
2018-09-05 18:48:19 -0400

    fix

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const socketIO = require('socket.io');
 const path = require('path');
 
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-});
+const server = express()
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.listen(process.env.PORT, () => {
-  console.log('Server ON, port:', process.env.PORT);
-  console.log('The environment is', process.env.NODE_ENV);
-});
-
-const io = socketIO(app);
+const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
