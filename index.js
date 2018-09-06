087faee49d2dd037443c9481e6dea498e2c46120
mauricepoirrier <mauricepoirrier@gmail.com>
2018-09-06 17:23:21 -0300

    Adding post and get routes

--- index.js
+++ index.js
@@ -8,21 +8,9 @@ const PORT = process.env.PORT || 3000;
 const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
-
-var count = 0;
+var count = 0
 
 server.use(bodyParser.json());
-
-
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  count++;
-  io.emit('alert', {meg: count});
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
 server.get('/',function (req,res)  {
   console.log('Recieved Get');
   res.send('Hello');
@@ -32,3 +20,15 @@ server.post('/', function (req, res) {
   count++;
   console.log('Recieved post')
 });
+
+const io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  count++;
+  io.emit('alert', {meg: count});
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+
