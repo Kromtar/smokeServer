069be325599e10186093d2971e77568f1a153d85
mauricepoirrier <mauricepoirrier@gmail.com>
2018-09-06 17:16:28 -0300

    Adding post and get routes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var count = 0;
 const app = express()
 app.use(bodyParser.json());
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!');
 });
 
