c0eda2ece1b1f0bccfe273da3e3e9011707e464e
mauricepoirrier <mauricepoirrier@gmail.com>
2018-09-11 20:08:06 -0300

    post added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 //Formato de consultas
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.text);
+app.use(bodyParser.text());
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
