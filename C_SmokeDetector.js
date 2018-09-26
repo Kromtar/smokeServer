4990a2f982ddd5aa9645a8f804195d4b5ac1fc11
Vicente Ternicien <38274115+vicho2t@users.noreply.github.com>
2018-09-25 22:28:41 -0300

    ClassUpdate

--- C_SmokeDetector.js
+++ C_SmokeDetector.js
@@ -1,9 +1,30 @@
+
 class SmokeDetector {
-  constructor(kitId, kitName, userId, ) {	
+  constructor(kitId, kitName, userId, numberOfSensors) {	
 
- this.kitId = kitId;
- 
+ 	this.kitId = kitId;
+ 	this.kitName = kitName;
+ 	this.userId = userId;
+ 	this.numberOfSensors = numberOfSensors;
+ 	var status = "NORMAL";
+ 	var sensors = [];
 
+ 	while(numberOfSensors > 0){
+ 		sensors.push(numberOfSensors);
+ 		numberOfSensors--;
+ 	} 
 
-  }
+ 	
+/*
+   }
+   //Return an object for testing purposes
+   constructor(){
+   	var kitId = 1;
+    var kitName = "test";
+    var userId = 1;
+    var status = "NORMAL";
+
+   }
+*/
+
 }
